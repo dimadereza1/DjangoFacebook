a4520bee19f8a6c462aea5c04c3119398f83a28e
I_am_jumper_My_job_is__to_jump <dimadereza1@gmail.com>
2023-07-14 19:51:36 +0300

    Header (base.html): added a header for the site

--- facebook/facebook_app/models.py
+++ facebook/facebook_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Abstract(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class PostM(Abstract):
+    text = models.TextField()
+    images = models.ImageField(upload_to='media/post_img/')
+    city = models.TextField()
+
+class ProfileM(Abstract):
+    avatar = models.ImageField(upload_to='media/profile/')
+
--- facebook/facebook_app/views.py
+++ facebook/facebook_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from .models import PostM, User
+from django.core.files import File
+from pathlib import Path
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+class Logout(LoginView):
+    pass
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def post(self, **kwargs):
+        return JsonResponse('ok', safe=False)
